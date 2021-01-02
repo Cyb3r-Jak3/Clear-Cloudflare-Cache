ac427fce92d47deb649bde35ba7a79deb40f5895
Cyb3r Jak3 <jake@jwhite.network>
2021-01-02 12:06:06 -0800

    Lots of details (#1)
    * Adding in tests
    
    * Adds some more details to README
    
    * Added more documentation and License
    
    * Adds in legacy auth

--- entrypoint.sh
+++ entrypoint.sh
@@ -11,48 +11,51 @@ usesBoolean() {
 }
 
 main() {
-    if ! uses "$ZONE"; then
-        echo "ZONE is not set. Quitting."
+    if ! uses "${INPUT_ZONE}"; then
+        echo "Zone is not set and is required."
         exit 1
     fi
 
-    if uses "$GLOBAL_KEY"; then
-        if uses "$EMAIL"; then
+    if uses "${INPUT_GLOBAL_TOKEN}"; then
+        if uses "${INPUT_EMAIL}"; then
             method="legacy"
         else
             echo "Need email when using Cloudflare Global Key"
             exit 1
         fi
-    elif uses "$API_TOKEN"; then
+    elif uses "${INPUT_API_TOKEN}"; then
         method="token"
     else
         echo "No Authentication Method was given."
+        exit 1
     fi
 
-    if uses "$URLS"; then
-        set -- --data '{"files":'"${URLS}"'}'
+    if uses "${INPUT_URLS}"; then
+        urlInput='{"files":'"${INPUT_URLS}"'}'
     else
-        set -- --data '{"purge_everything":true}'
+        urlInput='{"purge_everything":true}'
     fi
     if [ "$method" = "legacy" ]; then
-        RESPONSE=$(curl -sS "https://api.cloudflare.com/client/v4/zones/${ZONE}/purge_cache" \
+        RESPONSE=$(curl -sS "https://api.cloudflare.com/client/v4/zones/${INPUT_ZONE}/purge_cache" \
                       -H "Content-Type: application/json" \
-                      -H "X-Auth-Email: ${EMAIL}" \
-                      -H "X-Auth-Key: ${GLOBAL_KEY}" \
+                      -H "X-Auth-Email: ${INPUT_EMAIL}" \
+                      -H "X-Auth-Key: ${INPUT_GLOBAL_TOKEN}" \
                       -w "HTTP_STATUS:%{http_code}" \
-                      "$@")
-    elif [ "$API_METHOD" -eq 2 ]; then
-        RESPONSE=$(curl -sS "https://api.cloudflare.com/client/v4/zones/${ZONE}/purge_cache" \
+                      --data "${urlInput}"
+                      )
+    elif [ "$method" = "token" ]; then
+        RESPONSE=$(curl -sS "https://api.cloudflare.com/client/v4/zones/${INPUT_ZONE}/purge_cache" \
                       -H "Content-Type: application/json" \
-                      -H "Authorization: Bearer ${API_TOKEN}" \
+                      -H "Authorization: Bearer ${INPUT_API_TOKEN}" \
                       -w "HTTP_STATUS:%{http_code}" \
-                      "$@")
+                      --data "${urlInput}"
+                      )
     fi
     BODY=$(echo "${RESPONSE}" | sed -E 's/HTTP_STATUS\:[0-9]{3}$//')
     STATUS=$(echo "${RESPONSE}" | tr -d '\n' | sed -E 's/.*HTTP_STATUS:([0-9]{3})$/\1/')
 
     if [ "${STATUS}" -eq "200" ]; then
-        echo "Successfully purged!"
+        echo "Successfully cleared!"
         exit 0
     else
         echo "Purge failed. HTTP Code: ${STATUS} API response was: "
@@ -61,3 +64,4 @@ main() {
     fi
 }
 
+main
